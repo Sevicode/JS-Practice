7bd2c4ec4b18687d3265f39a20a7ed71246697d8
Sevicode <41233053+Sevicode@users.noreply.github.com>
2020-09-27 23:00:55 +1000

    creating dynamic error messages

---
+++ formValidator/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// we need a event listener for our form 
+//to listion for 'submit' and then run a 'function':
+
+form.addEventListener('submit', function (e) {
+
+});
+
