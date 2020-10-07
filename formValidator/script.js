c76b5e28f3ec2b83a31ec3d43bf2049553feb6c9
Sevicode <41233053+Sevicode@users.noreply.github.com>
2020-10-07 21:56:33 +1000

    simple form validations added

--- formValidator/script.js
+++ formValidator/script.js
@@ -4,10 +4,66 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//Show input error message
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small')
+    small.innerText = message;
+}
+
+//show success outline
+
+function showSuccess(input) {
+    const formControl.className = 'form-control success';
+}
+
+
+//check if email is valid
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
 // we need a event listener for our form 
 //to listion for 'submit' and then run a 'function':
 
 form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (username.value === "") {
+        showError(username, 'Username is required');
+        
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === "") {
+        showError(email, 'Email is required');
+
+    } else if (!validateEmail(email.value)) {
+        showError(email, 'Email is not valid!')
+        
+    }else {
+        showSuccess(email);
+    }
+
+    if (password.value === "") {
+        showError(password, 'Password is required');
+
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === "") {
+        showError(password2, 'Password is required');
+
+    } else {
+        showSuccess(password2);
+    }
 
 });
 
