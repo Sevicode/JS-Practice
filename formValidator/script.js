f5b4ef042b06c58b29d8317912b2545c7647cc0b
Sevicode <41233053+Sevicode@users.noreply.github.com>
2020-10-07 22:23:42 +1000

    check required/refactored previous conditionals

--- formValidator/script.js
+++ formValidator/script.js
@@ -8,25 +8,50 @@ const password2 = document.getElementById('password2');
 
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
 //show success outline
 
 function showSuccess(input) {
-    const formControl.className = 'form-control success';
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
 
 
 //check if email is valid
 
-function validateEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
+
+// Check required fields
+
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+            
+        }
+        else {
+            showSuccess(input)
+        }  
+    });
+    
+}
+
+//get fieldname 
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 // we need a event listener for our form 
 //to listion for 'submit' and then run a 'function':
@@ -34,36 +59,6 @@ function validateEmail(email) {
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    if (username.value === "") {
-        showError(username, 'Username is required');
-        
-    } else {
-        showSuccess(username);
-    }
-
-    if (email.value === "") {
-        showError(email, 'Email is required');
-
-    } else if (!validateEmail(email.value)) {
-        showError(email, 'Email is not valid!')
-        
-    }else {
-        showSuccess(email);
-    }
-
-    if (password.value === "") {
-        showError(password, 'Password is required');
-
-    } else {
-        showSuccess(password);
-    }
-
-    if (password2.value === "") {
-        showError(password2, 'Password is required');
-
-    } else {
-        showSuccess(password2);
-    }
-
+    checkRequired([username, email, password, password2])
 });
 
